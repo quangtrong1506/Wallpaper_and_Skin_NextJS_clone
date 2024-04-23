ebfa8a4d9968ca98590cc26cec279cc62a73de23
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2024-04-23 17:32:22 +0700

    clone màn hình game

--- renderer/pages/screen-game.tsx
+++ renderer/pages/screen-game.tsx
@@ -1,14 +1,33 @@
-import Valkyrie from "../Components/Game/Valkyrie";
+import { useEffect, useState } from "react";
+import HexagonBig from "../Components/Game/HexagonBig";
 
 function ScreenGamePage() {
+    const [scale, setScale] = useState(1);
+    useEffect(() => {
+        let w = document.body.clientWidth;
+    }, []);
     return (
-        <div className="relative">
-            <div className="overflow-hidden h-screen">
+        <div className="relative ">
+            <div className="overflow-hidden">
                 <video muted autoPlay loop src="/videos/bg-default.mp4"></video>
+                {/* <img src="/images/abc.png" alt="" /> */}
             </div>
-            <div className="absolute top-0 left-0 w"></div>
-            <div className="absolute bottom-10 left-10 w-60">
-                <Valkyrie />
+            <div className="absolute top-0 left-0"></div>
+            <div className={`absolute h-[655px] w-[490px] top-[19vh] right-[1.2vw] scale-[${scale}]`}>
+                <div className="relative">
+                    <div className="absolute -top-2 w-[208px] right-[48px] text-[28px]">
+                        <HexagonBig text="Trang Bị" type="valkyrie" />
+                    </div>
+                    <div className="absolute top-24 w-[208px] right-[210px] text-[26px]">
+                        <HexagonBig text="Người Cộng Tác" type="nguoi" />
+                    </div>
+                    <div className="absolute top-[280px] right-[222px] w-[260px] text-[28px]">
+                        <HexagonBig text="Valkyrie" type="valkyrie" />
+                    </div>
+                    <div className="absolute top-[428px] right-[28px] w-[260px] text-[28px]">
+                        <HexagonBig text="Tiếp Tế" type="box" />
+                    </div>
+                </div>
             </div>
         </div>
     );

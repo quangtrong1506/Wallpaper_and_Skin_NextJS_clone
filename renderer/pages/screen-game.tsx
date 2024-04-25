ba3e9d76fa6a3c0402921c231860c47d80c7d948
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2024-04-25 17:42:37 +0700

    thêm 2 cái nút bên phải dưới

--- renderer/pages/screen-game.tsx
+++ renderer/pages/screen-game.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
-import HexagonBig from "../Components/Game/HexagonBig";
+import { useEffect, useState } from 'react';
+import BottomButton from '../Components/Game/Screen/BottomButton';
+import HexagonBig from '../Components/Game/Screen/HexagonBig';
+import Start from '../Components/Game/Screen/Start';
 
 function ScreenGamePage() {
     const [scale, setScale] = useState(1);
@@ -10,20 +12,29 @@ function ScreenGamePage() {
             console.log(clientWidth / 1920);
         };
         reSize();
-        window.addEventListener("resize", reSize);
+        window.addEventListener('resize', reSize);
         return () => {
-            window.removeEventListener("resize", reSize);
+            window.removeEventListener('resize', reSize);
         };
     }, []);
     return (
-        <div className="relative ">
+        <div className="relative overflow-hidden">
             <div className="overflow-hidden">
                 {/* <video muted autoPlay loop src="/videos/hk-3.mp4"></video> */}
                 <img src="/images/abc.png" alt="" />
             </div>
             <div className="absolute top-0 left-0"></div>
-            <div className="absolute h-[655px] w-[490px] top-[19vh] right-[1.2vw]">
-                <div className="relative" style={{ scale: `${scale}` }}>
+            <div
+                className="absolute"
+                style={{
+                    width: 490 * scale + 'px',
+                    height: 655 * scale + 'px',
+                    top: 19.1 * scale + 'vh',
+                    right: 1.2 * scale + 'vw',
+                    transform: `translateX(${((1 - scale) * 490 * scale) / 2.18}px) translateY(-${((1 - scale) * 655 * scale) / 4.95}px) `,
+                }}
+            >
+                <div className="relative w-full h-full" style={{ scale: `${scale}` }}>
                     <div className="absolute -top-2 w-[208px] right-[48px] text-[28px]">
                         <HexagonBig text="Trang Bị" type="valkyrie" />
                     </div>
@@ -34,8 +45,27 @@ function ScreenGamePage() {
                         <HexagonBig text="Valkyrie" type="valkyrie" />
                     </div>
                     <div className="absolute top-[428px] right-[28px] w-[260px] text-[28px]">
-                        <HexagonBig text="Tiếp Tế" type="box" />
+                        <HexagonBig text="Ứng dụng" type="box" />
                     </div>
+                    <div className="absolute top-[195px] right-[3px] w-[260px]">
+                        <Start />
+                    </div>
+                </div>
+            </div>
+            <div
+                className="absolute flex "
+                style={{
+                    width: 620 * scale + 'px',
+                    height: 82 * scale + 'px',
+                    bottom: 2.2 * scale + 'vw',
+                    right: 2.2 * scale + 'vw',
+                }}
+            >
+                <div>
+                    <BottomButton scale={scale} icon={0} text="Hải Quân" />
+                </div>
+                <div className="ms-5">
+                    <BottomButton scale={scale} icon={1} text="Gia Viên" />
                 </div>
             </div>
         </div>
